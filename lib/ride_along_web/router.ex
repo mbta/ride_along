1682dcc1b9fe9d6e4bbe9859142d4a1cda2be389
Paul Swartz <paul@paulswartz.net>
2024-05-22 13:12:19 -0400

    fix: content security policy

--- lib/ride_along_web/router.ex
+++ lib/ride_along_web/router.ex
@@ -8,7 +8,10 @@ defmodule RideAlongWeb.Router do
     plug :fetch_live_flash
     plug :put_root_layout, html: {RideAlongWeb.Layouts, :root}
     plug :protect_from_forgery
-    plug :put_secure_browser_headers
+
+    plug :put_secure_browser_headers, %{
+      "content-security-policy" => "default-src 'self'; img-src https://cdn.mbta.com data: 'self'"
+    }
   end
 
   pipeline :api do
