bbbe6336b043aceffeb35750de82939e3aecab5a
Paul Swartz <paul@paulswartz.net>
2024-05-21 21:46:44 -0400

    feat: send destination/vehicle to the map

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,59 +18,83 @@
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html"
 // Establish Phoenix Socket and LiveView configuration.
-import {Socket} from "phoenix"
-import {LiveSocket} from "phoenix_live_view"
+import { Socket } from "phoenix"
+import { LiveSocket } from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 // Leaflet for maps
-import "leaflet";
-import "leaflet-rotatedmarker";
+import "leaflet"
+import "leaflet-rotatedmarker"
 
 let locationIcon = L.icon({
   iconUrl: "/images/icon-circle-locations-default.svg",
   iconAnchor: [15, 15],
   iconSize: [30, 30],
-});
+})
 
 let vehicleIcon = L.icon({
   iconUrl: "/images/icon-vehicle-bordered.svg",
   iconAnchor: [20, 20],
   iconSize: [40, 40],
-});
+})
 
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
+let csrfToken = document
+  .querySelector("meta[name='csrf-token']")
+  .getAttribute("content")
 let Hooks = {}
 Hooks.Leaflet = {
   mounted() {
-    const centerOfBoston = [42.3516728,-71.0718109]
-    this.map = L.map(this.el).setView(centerOfBoston, 15);
-    L.tileLayer('https://cdn.mbta.com/osm_tiles/{z}/{x}/{y}.png', {
+    const centerOfBoston = [42.3516728, -71.0718109]
+    this.map = L.map(this.el).setView(centerOfBoston, 15)
+    L.tileLayer("https://cdn.mbta.com/osm_tiles/{z}/{x}/{y}.png", {
       maxZoom: 19,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(this.map);
-    L.marker(centerOfBoston,
-             {
-               icon: locationIcon,
-               alt: 'Boston, MA'
-             }).addTo(this.map);
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(this.map)
 
-    L.marker([42.3516768,-71.0695149],
-             {
-               icon: vehicleIcon,
-               rotationOrigin: "center center",
-               rotationAngle: 65
-             }).addTo(this.map);
-  }
+    this.handleEvent("destination", (d) => {
+      let location = [d.lat, d.lon]
+      if (this.destination) {
+        this.destination.setLatLng(location)
+      } else {
+        this.destination = L.marker(location, {
+          icon: locationIcon,
+          alt: d.alt,
+          draggable: true,
+          autoPan: true,
+        })
+          .addTo(this.map)
+          .on("move", (e) => this.pushEvent("destination-moved", e.latlng))
+      }
+    })
+
+    this.handleEvent("vehicle", (v) => {
+      let location = [v.lat, v.lon]
+      if (this.vehicle) {
+        this.vehicle.setLatLng(location)
+      } else {
+        this.vehicle = L.marker(location, {
+          icon: vehicleIcon,
+          rotationOrigin: "center center",
+          rotationAngle: 65,
+          draggable: true,
+          autoPan: true,
+        })
+          .addTo(this.map)
+          .on("move", (e) => this.pushEvent("vehicle-moved", e.latlng))
+      }
+    })
+  },
 }
 let liveSocket = new LiveSocket("/live", Socket, {
   longPollFallbackMs: 2500,
-  params: {_csrf_token: csrfToken},
-  hooks: Hooks
+  params: { _csrf_token: csrfToken },
+  hooks: Hooks,
 })
 
 // Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
-window.addEventListener("phx:page-loading-start", _info => topbar.show(300))
-window.addEventListener("phx:page-loading-stop", _info => topbar.hide())
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" })
+window.addEventListener("phx:page-loading-start", (_info) => topbar.show(300))
+window.addEventListener("phx:page-loading-stop", (_info) => topbar.hide())
 
 // connect if there are any LiveViews on the page
 liveSocket.connect()
@@ -80,4 +104,3 @@ liveSocket.connect()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
-
--- lib/ride_along_web/live/trip_live/show.ex
+++ lib/ride_along_web/live/trip_live/show.ex
@@ -1,9 +1,22 @@
 defmodule RideAlongWeb.TripLive.Show do
   use RideAlongWeb, :live_view
 
+  @destination %{
+    alt: "Boston, MA",
+    lat: 42.3516728,
+    lon: -71.0718109
+  }
+  @vehicle %{
+    lat: 42.3516768,
+    lon: -71.0695149
+  }
+
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok,
+     socket
+     |> push_event("destination", @destination)
+     |> push_event("vehicle", @vehicle)}
   end
 
   @impl true
@@ -11,6 +24,13 @@ defmodule RideAlongWeb.TripLive.Show do
     {:noreply,
      socket
      |> assign(:page_title, "Track your Trip")
+     |> assign(:vehicle, @vehicle)
+     |> assign(:destination, @destination)
      |> assign(:trip, nil)}
+  end
+
+  @impl true
+  def handle_event(_event, _data, socket) do
+    {:noreply, socket}
   end
 end
