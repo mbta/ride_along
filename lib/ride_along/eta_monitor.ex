daa050edf0ec024f3dea0b522d58bc9db7af2007
Paul Swartz <paul@paulswartz.net>
2024-06-06 13:59:06 -0400

    feat(EtaMonitor): only log when the status or pick_time change

--- lib/ride_along/eta_monitor.ex
+++ lib/ride_along/eta_monitor.ex
@@ -18,53 +18,92 @@ defmodule RideAlong.EtaMonitor do
     end
   end
 
-  defstruct []
+  defstruct trip_date_to_key: %{}
+
+  @type trip_date :: {Adept.Trip.id(), Date.t()}
+  @type key :: {pick_time :: DateTime.t(), status :: Adept.Trip.status()}
 
   @impl GenServer
   def init(_) do
     state = update_trips(%__MODULE__{})
     Phoenix.PubSub.subscribe(RideAlong.PubSub, "trips:updated")
+    Phoenix.PubSub.subscribe(RideAlong.PubSub, "vehicles:updated")
 
     {:ok, state}
   end
 
   @impl GenServer
   def handle_info(:trips_updated, state) do
-    state = update_trips(state)
+    state =
+      state
+      |> clean_state()
+      |> update_trips()
+
     {:noreply, state}
   end
 
-  def update_trips(state) do
-    now = DateTime.utc_now()
+  def handle_info(:vehicles_updated, state) do
+    state = update_trips(state)
 
-    for trip <- Enum.sort(Adept.all_trips(), Adept.Trip),
-        vehicle = Adept.get_vehicle_by_route(trip.route_id),
-        status = Adept.Trip.status(trip, vehicle, now),
-        status != :closed do
-      route =
-        if status not in [:arrived, :picked_up] and is_map(vehicle) do
-          case RideAlong.OpenRouteService.directions(trip, vehicle) do
-            {:ok, route} -> route
-            _ -> nil
-          end
-        else
-          nil
-        end
+    {:noreply, state}
+  end
 
-      ors_eta =
-        if route do
-          DateTime.to_iso8601(
-            DateTime.add(vehicle.timestamp, trunc(route.duration * 1000), :millisecond)
-          )
-        else
-          nil
-        end
+  def clean_state(state, today \\ Date.utc_today()) do
+    trip_date_to_key =
+      for {{trip_id, date}, value} <- state.trip_date_to_key,
+          Date.diff(date, today) >= -1,
+          into: %{} do
+        {{trip_id, date}, value}
+      end
 
-      Logger.info(
-        "#{__MODULE__} trip_id=#{trip.trip_id} route=#{trip.route_id} status=#{status} promise=#{DateTime.to_iso8601(trip.promise_time)} pick=#{DateTime.to_iso8601(trip.pick_time)} ors_eta=#{ors_eta}"
-      )
+    %{state | trip_date_to_key: trip_date_to_key}
+  end
+
+  def update_trips(state, trips \\ Adept.all_trips(), now \\ DateTime.utc_now()) do
+    trips
+    |> Enum.sort(Adept.Trip)
+    |> Enum.reduce(state, &update_trip(&2, &1, now))
+  end
+
+  def update_trip(state, trip, now) do
+    vehicle = Adept.get_vehicle_by_route(trip.route_id)
+    status = Adept.Trip.status(trip, vehicle, now)
+    trip_date = {trip.trip_id, trip.date}
+
+    new_key = {trip.pick_time, status}
+    old_key = Map.get(state.trip_date_to_key, trip_date, new_key)
+
+    state = put_in(state.trip_date_to_key[trip_date], new_key)
+
+    if new_key != old_key do
+      log_trip_status_change(trip, vehicle, status)
     end
 
     state
+  end
+
+  def log_trip_status_change(trip, vehicle, status) do
+    route =
+      if status not in [:arrived, :picked_up, :closed] and is_map(vehicle) do
+        case RideAlong.OpenRouteService.directions(trip, vehicle) do
+          {:ok, route} -> route
+          _ -> nil
+        end
+      else
+        nil
+      end
+
+    ors_eta =
+      if route do
+        DateTime.to_iso8601(
+          DateTime.add(vehicle.timestamp, trunc(route.duration * 1000), :millisecond)
+        )
+      else
+        nil
+      end
+
+    Logger.info(
+      "#{__MODULE__} trip_id=#{trip.trip_id} route=#{trip.route_id} status=#{status} promise=#{DateTime.to_iso8601(trip.promise_time)} pick=#{DateTime.to_iso8601(trip.pick_time)} ors_eta=#{ors_eta}"
+    )
   end
 end
