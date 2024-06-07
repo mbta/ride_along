c362530eba40f4c97f5276f21f4e6cc27dccdfa2
Paul Swartz <paul@paulswartz.net>
2024-06-07 15:36:21 -0400

    feat: keep track of the last ORS ETA
    We then use it for the arrived/picked_up states, which otherwise would
    have an ETA of right now.

--- lib/ride_along/eta_monitor.ex
+++ lib/ride_along/eta_monitor.ex
@@ -18,7 +18,7 @@ defmodule RideAlong.EtaMonitor do
     end
   end
 
-  defstruct trip_date_to_key: %{}
+  defstruct trip_date_to_key: %{}, latest_ors_eta: %{}
 
   @type trip_date :: {Adept.Trip.id(), Date.t()}
   @type key :: {pick_time :: DateTime.t(), status :: Adept.Trip.status()}
@@ -56,7 +56,15 @@ defmodule RideAlong.EtaMonitor do
         {{trip_id, date}, value}
       end
 
-    %{state | trip_date_to_key: trip_date_to_key}
+    trip_ids =
+      trip_date_to_key
+      |> Map.keys()
+      |> MapSet.new(&elem(&1, 0))
+
+    latest_ors_eta =
+      Map.filter(state.latest_ors_eta, fn {trip_id, _} -> MapSet.member?(trip_ids, trip_id) end)
+
+    %{state | trip_date_to_key: trip_date_to_key, latest_ors_eta: latest_ors_eta}
   end
 
   def update_trips(state, trips \\ Adept.all_trips(), now \\ DateTime.utc_now()) do
@@ -76,13 +84,13 @@ defmodule RideAlong.EtaMonitor do
     state = put_in(state.trip_date_to_key[trip_date], new_key)
 
     if new_key != old_key do
-      log_trip_status_change(trip, vehicle, status)
+      log_trip_status_change(state, trip, vehicle, status)
+    else
+      state
     end
-
-    state
   end
 
-  def log_trip_status_change(trip, vehicle, status) do
+  def log_trip_status_change(state, trip, vehicle, status) do
     route =
       if status not in [:arrived, :picked_up, :closed] and is_map(vehicle) do
         case RideAlong.OpenRouteService.directions(trip, vehicle) do
@@ -99,11 +107,13 @@ defmodule RideAlong.EtaMonitor do
           DateTime.add(vehicle.timestamp, trunc(route.duration * 1000), :millisecond)
         )
       else
-        nil
+        state.latest_ors_eta[trip.trip_id]
       end
 
     Logger.info(
       "#{__MODULE__} trip_id=#{trip.trip_id} route=#{trip.route_id} status=#{status} promise=#{DateTime.to_iso8601(trip.promise_time)} pick=#{DateTime.to_iso8601(trip.pick_time)} ors_eta=#{ors_eta}"
     )
+
+    put_in(state.latest_ors_eta[trip.trip_id], ors_eta)
   end
 end
--- test/ride_along/eta_monitor_test.exs
+++ test/ride_along/eta_monitor_test.exs
@@ -4,36 +4,50 @@ defmodule RideAlong.EtaMonitorTest do
   import ExUnit.CaptureLog
 
   alias RideAlong.Adept
+  alias RideAlong.AdeptFixtures
   alias RideAlong.EtaMonitor
+  alias RideAlong.OpenRouteServiceFixtures, as: ORSFixtures
 
   describe "update_trips/2" do
     setup do
+      today = ~D[2024-06-06]
+      now = ~U[2024-06-06T13:47:00Z]
       route_id = 2345
 
       Adept.set_vehicles([
-        %Adept.Vehicle{route_id: route_id, vehicle_id: "2345", last_pick: 5, last_drop: 6}
+        AdeptFixtures.vehicle_fixture(%{
+          route_id: route_id,
+          vehicle_id: "2345",
+          last_pick: 5,
+          last_drop: 6,
+          timestamp: now
+        })
       ])
 
       state = %EtaMonitor{}
 
-      {:ok, route_id: route_id, state: state}
+      {:ok, route_id: route_id, today: today, now: now, state: state}
     end
 
-    test "logs a message when the trip status changes", %{route_id: route_id, state: state} do
-      today = ~D[2024-06-06]
-      now = ~U[2024-06-06T13:47:00Z]
+    test "logs a message when the trip status changes", %{
+      route_id: route_id,
+      today: today,
+      now: now,
+      state: state
+    } do
       pick_time = ~U[2024-06-06T14:00:00Z]
 
-      trip = %Adept.Trip{
-        trip_id: 1,
-        route_id: route_id,
-        date: today,
-        pick_time: pick_time,
-        promise_time: pick_time,
-        pick_order: 5,
-        drop_order: 9,
-        pickup_performed?: true
-      }
+      trip =
+        AdeptFixtures.trip_fixture(%{
+          trip_id: 1,
+          route_id: route_id,
+          date: today,
+          pick_time: pick_time,
+          promise_time: pick_time,
+          pick_order: 5,
+          drop_order: 9,
+          pickup_performed?: true
+        })
 
       state = EtaMonitor.update_trips(state, [trip], now)
 
@@ -53,6 +67,49 @@ defmodule RideAlong.EtaMonitorTest do
 
       assert log =~ "EtaMonitor"
     end
+
+    test "keeps track of the last ORS eta, uses it in the arrival/pickup log", %{
+      route_id: route_id,
+      today: today,
+      now: now,
+      state: state
+    } do
+      pick_time = ~U[2024-06-06T14:00:00Z]
+
+      trip =
+        AdeptFixtures.trip_fixture(%{
+          trip_id: 1,
+          route_id: route_id,
+          date: today,
+          pick_time: pick_time,
+          promise_time: pick_time,
+          pick_order: 8,
+          drop_order: 9
+        })
+
+      # enqueued
+
+      state = EtaMonitor.update_trips(state, [trip], now)
+
+      # enroute
+      trip = %{trip | pick_order: 7}
+
+      stub_ors!()
+      state = EtaMonitor.update_trips(state, [trip], now)
+
+      # picked_up
+      trip = %{trip | pick_order: 6, pickup_performed?: true}
+
+      log_level!(:info)
+
+      log =
+        capture_log(fn ->
+          EtaMonitor.update_trips(state, [trip], now)
+        end)
+
+      assert log =~ "EtaMonitor"
+      assert log =~ "ors_eta=2024-06-06"
+    end
   end
 
   describe "clean_state/2" do
@@ -68,6 +125,11 @@ defmodule RideAlong.EtaMonitorTest do
           {2, today} => {},
           {3, yesterday} => {},
           {4, day_before_yesterday} => {}
+        },
+        latest_ors_eta: %{
+          2 => "",
+          3 => "",
+          4 => ""
         }
       }
 
@@ -78,7 +140,13 @@ defmodule RideAlong.EtaMonitorTest do
                {2, today},
                {3, yesterday}
              ]
+
+      assert Enum.sort(Map.keys(state.latest_ors_eta)) == [2, 3]
     end
+  end
+
+  def stub_ors! do
+    ORSFixtures.stub(ORSFixtures.fixture())
   end
 
   def log_level!(level) do
