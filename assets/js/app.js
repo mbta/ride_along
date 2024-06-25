771952ab65a38316619503a53455f325b4ce2902
Paul Swartz <paul@paulswartz.net>
2024-06-25 12:38:37 -0400

    ci: switch from Prettier -> StandardJS

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,115 +16,114 @@
 //
 
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
-import "phoenix_html";
+import 'phoenix_html'
 // Establish Phoenix Socket and LiveView configuration.
-import { Socket } from "phoenix";
-import { LiveSocket } from "phoenix_live_view";
-import topbar from "../vendor/topbar";
-import _ from "lodash";
+import { Socket } from 'phoenix'
+import { LiveSocket } from 'phoenix_live_view'
+import topbar from '../vendor/topbar'
 
 // Leaflet for maps
-import "leaflet";
-import "leaflet-rotatedmarker";
-import polyline from "polyline-encoded";
+import * as L from 'leaflet'
+import 'leaflet-rotatedmarker'
+import polyline from 'polyline-encoded'
 
-let locationIcon = L.icon({
-  iconUrl: "/images/icon-circle-locations-default.svg",
+const locationIcon = L.icon({
+  iconUrl: '/images/icon-circle-locations-default.svg',
   iconAnchor: [15, 15],
-  iconSize: [30, 30],
-});
+  iconSize: [30, 30]
+})
 
-let vehicleIcon = L.icon({
-  iconUrl: "/images/icon-vehicle-bordered.svg",
+const vehicleIcon = L.icon({
+  iconUrl: '/images/icon-vehicle-bordered.svg',
   iconAnchor: [20, 20],
-  iconSize: [40, 40],
-});
+  iconSize: [40, 40]
+})
 
-let csrfToken = document
+const csrfToken = document
   .querySelector("meta[name='csrf-token']")
-  .getAttribute("content");
-let Hooks = {};
+  .getAttribute('content')
+const Hooks = {}
 Hooks.Leaflet = {
-  mounted() {
-    const centerOfBoston = [42.3516728, -71.0718109];
-    this.map = L.map(this.el).setView(centerOfBoston, 15);
-    L.tileLayer("https://cdn.mbta.com/osm_tiles/{z}/{x}/{y}.png", {
+  mounted () {
+    const centerOfBoston = [42.3516728, -71.0718109]
+    this.map = L.map(this.el).setView(centerOfBoston, 15)
+    L.tileLayer('https://cdn.mbta.com/osm_tiles/{z}/{x}/{y}.png', {
       maxZoom: 18,
       minZoom: 9,
       attribution:
-        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    }).addTo(this.map);
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(this.map)
 
-    let destination = JSON.parse(this.el.dataset.destination);
-    let vehicle = this.el.dataset.vehicle;
+    const destination = JSON.parse(this.el.dataset.destination)
+    const vehicle = this.el.dataset.vehicle
 
     this.destination = L.marker([destination.lat, destination.lon], {
       icon: locationIcon,
       alt: destination.alt,
       interactive: false,
-      keyboard: false,
-    }).addTo(this.map);
+      keyboard: false
+    }).addTo(this.map)
 
-    this.map.fitBounds([destination, destination], { padding: [5, 5] });
+    this.map.fitBounds([destination, destination], { padding: [5, 5] })
 
-    this.handleEvent("route", (r) => {
-      let decoded = polyline.decode(r.polyline);
-      let location = decoded[0];
+    this.handleEvent('route', (r) => {
+      const decoded = polyline.decode(r.polyline)
+      const location = decoded[0]
 
       if (this.vehicle) {
-        this.vehicle.setLatLng(location).setRotationAngle(r.bearing);
+        this.vehicle.setLatLng(location).setRotationAngle(r.bearing)
       } else {
         this.vehicle = L.marker(location, {
           icon: vehicleIcon,
           alt: vehicle,
-          rotationOrigin: "center center",
+          rotationOrigin: 'center center',
           rotationAngle: r.bearing,
           interactive: false,
-          keyboard: false,
-        }).addTo(this.map);
+          keyboard: false
+        }).addTo(this.map)
       }
 
       if (this.polyline) {
-        this.polyline.setLatLngs(decoded);
+        this.polyline.setLatLngs(decoded)
       } else {
         this.polyline = L.polyline(decoded, {
-          color: "blue",
-          interactive: false,
-        }).addTo(this.map);
+          color: 'blue',
+          interactive: false
+        }).addTo(this.map)
       }
 
-      this.map.fitBounds(r.bbox, { padding: [5, 5] });
-    });
-    this.handleEvent("clearRoute", () => {
+      this.map.fitBounds(r.bbox, { padding: [5, 5] })
+    })
+    this.handleEvent('clearRoute', () => {
       if (this.vehicle) {
-        this.map.removeLayer(this.vehicle);
+        this.map.removeLayer(this.vehicle)
       }
       if (this.polyline) {
-        this.map.removeLayer(this.polyline);
+        this.map.removeLayer(this.polyline)
       }
-      this.vehicle = this.polyline = null;
+      this.vehicle = this.polyline = null
 
       // recenter the map
-      this.map.fitBounds([destination, destination], { padding: [5, 5] });
-    });
-  },
-};
-let liveSocket = new LiveSocket("/live", Socket, {
+      this.map.fitBounds([destination, destination], { padding: [5, 5] })
+    })
+  }
+}
+const liveSocket = new LiveSocket('/live', Socket, {
   longPollFallbackMs: 2500,
   params: { _csrf_token: csrfToken },
-  hooks: Hooks,
-});
+  hooks: Hooks
+})
 
 // Show progress bar on live navigation and form submits
-topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" });
-window.addEventListener("phx:page-loading-start", (_info) => topbar.show(300));
-window.addEventListener("phx:page-loading-stop", (_info) => topbar.hide());
+topbar.config({ barColors: { 0: '#29d' }, shadowColor: 'rgba(0, 0, 0, .3)' })
+window.addEventListener('phx:page-loading-start', (_info) => topbar.show(300))
+window.addEventListener('phx:page-loading-stop', (_info) => topbar.hide())
 
 // connect if there are any LiveViews on the page
-liveSocket.connect();
+liveSocket.connect()
 
 // expose liveSocket on window for web console debug logs and latency simulation:
 // >> liveSocket.enableDebug()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
-window.liveSocket = liveSocket;
+window.liveSocket = liveSocket
--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,155 +1,155 @@
 // See the Tailwind configuration guide for advanced usage
 // https://tailwindcss.com/docs/configuration
 
-const plugin = require("tailwindcss/plugin");
-const { colors: defaultColors } = require("tailwindcss/defaultTheme");
-const fs = require("fs");
-const path = require("path");
+const plugin = require('tailwindcss/plugin')
+const { colors: defaultColors } = require('tailwindcss/defaultTheme')
+const fs = require('fs')
+const path = require('path')
 
 module.exports = {
   content: [
-    "./js/**/*.js",
-    "../lib/ride_along_web.ex",
-    "../lib/ride_along_web/**/*.*ex",
+    './js/**/*.js',
+    '../lib/ride_along_web.ex',
+    '../lib/ride_along_web/**/*.*ex'
   ],
   theme: {
     fontFamily: {
-      "inter-thin": ["InterThin"],
-      "inter-extralight": ["InterExtraLight"],
-      "inter-light": ["InterLight"],
-      "inter-normal": ["InterNormal"],
-      "inter-medium": ["InterMedium"],
-      "inter-semibold": ["InterSemiBold"],
-      "inter-bold": ["InterBold"],
-      "inter-extrabold": ["InterExtraBold"],
-      "inter-black": ["InterBlack"],
-      helvetica: ["Helvetica Neue", "system-ui", "sans-serif"],
-      sans: ["InterNormal", "system-ui", "sans-serif"],
+      'inter-thin': ['InterThin'],
+      'inter-extralight': ['InterExtraLight'],
+      'inter-light': ['InterLight'],
+      'inter-normal': ['InterNormal'],
+      'inter-medium': ['InterMedium'],
+      'inter-semibold': ['InterSemiBold'],
+      'inter-bold': ['InterBold'],
+      'inter-extrabold': ['InterExtraBold'],
+      'inter-black': ['InterBlack'],
+      helvetica: ['Helvetica Neue', 'system-ui', 'sans-serif'],
+      sans: ['InterNormal', 'system-ui', 'sans-serif']
     },
     extend: {
       colors: {
         ...defaultColors,
-        "mbta-blue": "#003DA5",
-        "mbta-green": "#00843D",
-        "mbta-red": "#DA291C",
-        "mbta-orange": "#ED8B00",
-        "mbta-silver": "#7C878E",
-        "mbta-purple": "#80276C",
-        "mbta-bus": "#FFC72C",
-        "mbta-ferry": "#008EAA",
-        "mbta-ride": "#59BEC9",
-        "mbta-swa": "#00B5E2",
-        "mbta-gray-dark": "#212322",
-        "mbta-winter-blue": "#13294B",
-        "mbta-midwinter-blue": "#147BD1",
-        "light-silver": "#D9D9D9",
+        'mbta-blue': '#003DA5',
+        'mbta-green': '#00843D',
+        'mbta-red': '#DA291C',
+        'mbta-orange': '#ED8B00',
+        'mbta-silver': '#7C878E',
+        'mbta-purple': '#80276C',
+        'mbta-bus': '#FFC72C',
+        'mbta-ferry': '#008EAA',
+        'mbta-ride': '#59BEC9',
+        'mbta-swa': '#00B5E2',
+        'mbta-gray-dark': '#212322',
+        'mbta-winter-blue': '#13294B',
+        'mbta-midwinter-blue': '#147BD1',
+        'light-silver': '#D9D9D9',
         blue: {
-          300: "#A1C6ED",
-          500: "#165C96",
-          600: "#092E4D",
+          300: '#A1C6ED',
+          500: '#165C96',
+          600: '#092E4D'
         },
         red: {
-          500: "#F2DEDE",
-          600: "#B3000F",
+          500: '#F2DEDE',
+          600: '#B3000F'
         },
         yellow: {
-          500: "#E59700",
+          500: '#E59700'
         },
         gray: {
-          200: "#E9EAED",
+          200: '#E9EAED'
         },
-        "callout-light-blue": "#CFE2FF",
-        "callout-light-yellow": "#FFF7BF",
-        "callout-light-green": "#DFF0D8",
-        "callout-light-red": "#F2DEDE",
-        "callout-outline-blue": "#B6D4FE",
-        "callout-outline-yellow": "#FFDD00",
-        "callout-outline-green": "#BADBCC",
-        "callout-outline-red": "#B3000F",
-        "callout-dark-blue": "#084298",
-        "callout-dark-green": "#145A06",
-        "callout-dark-red": "#B3000F",
-      },
-    },
+        'callout-light-blue': '#CFE2FF',
+        'callout-light-yellow': '#FFF7BF',
+        'callout-light-green': '#DFF0D8',
+        'callout-light-red': '#F2DEDE',
+        'callout-outline-blue': '#B6D4FE',
+        'callout-outline-yellow': '#FFDD00',
+        'callout-outline-green': '#BADBCC',
+        'callout-outline-red': '#B3000F',
+        'callout-dark-blue': '#084298',
+        'callout-dark-green': '#145A06',
+        'callout-dark-red': '#B3000F'
+      }
+    }
   },
   plugins: [
-    require("@tailwindcss/forms"),
+    require('@tailwindcss/forms'),
     // Allows prefixing tailwind classes with LiveView classes to add rules
     // only when LiveView classes are applied, for example:
     //
     //     <div class="phx-click-loading:animate-ping">
     //
     plugin(({ addVariant }) =>
-      addVariant("phx-no-feedback", [
-        ".phx-no-feedback&",
-        ".phx-no-feedback &",
-      ]),
+      addVariant('phx-no-feedback', [
+        '.phx-no-feedback&',
+        '.phx-no-feedback &'
+      ])
     ),
     plugin(({ addVariant }) =>
-      addVariant("phx-click-loading", [
-        ".phx-click-loading&",
-        ".phx-click-loading &",
-      ]),
+      addVariant('phx-click-loading', [
+        '.phx-click-loading&',
+        '.phx-click-loading &'
+      ])
     ),
     plugin(({ addVariant }) =>
-      addVariant("phx-submit-loading", [
-        ".phx-submit-loading&",
-        ".phx-submit-loading &",
-      ]),
+      addVariant('phx-submit-loading', [
+        '.phx-submit-loading&',
+        '.phx-submit-loading &'
+      ])
     ),
     plugin(({ addVariant }) =>
-      addVariant("phx-change-loading", [
-        ".phx-change-loading&",
-        ".phx-change-loading &",
-      ]),
+      addVariant('phx-change-loading', [
+        '.phx-change-loading&',
+        '.phx-change-loading &'
+      ])
     ),
 
     // Embeds Heroicons (https://heroicons.com) into your app.css bundle
     // See your `CoreComponents.icon/1` for more information.
     //
     plugin(function ({ matchComponents, theme }) {
-      let iconsDir = path.join(__dirname, "../deps/heroicons/optimized");
-      let values = {};
-      let icons = [
-        ["", "/24/outline"],
-        ["-solid", "/24/solid"],
-        ["-mini", "/20/solid"],
-        ["-micro", "/16/solid"],
-      ];
+      const iconsDir = path.join(__dirname, '../deps/heroicons/optimized')
+      const values = {}
+      const icons = [
+        ['', '/24/outline'],
+        ['-solid', '/24/solid'],
+        ['-mini', '/20/solid'],
+        ['-micro', '/16/solid']
+      ]
       icons.forEach(([suffix, dir]) => {
         fs.readdirSync(path.join(iconsDir, dir)).forEach((file) => {
-          let name = path.basename(file, ".svg") + suffix;
-          values[name] = { name, fullPath: path.join(iconsDir, dir, file) };
-        });
-      });
+          const name = path.basename(file, '.svg') + suffix
+          values[name] = { name, fullPath: path.join(iconsDir, dir, file) }
+        })
+      })
       matchComponents(
         {
           hero: ({ name, fullPath }) => {
-            let content = fs
+            const content = fs
               .readFileSync(fullPath)
               .toString()
-              .replace(/\r?\n|\r/g, "");
-            let size = theme("spacing.6");
-            if (name.endsWith("-mini")) {
-              size = theme("spacing.5");
-            } else if (name.endsWith("-micro")) {
-              size = theme("spacing.4");
+              .replace(/\r?\n|\r/g, '')
+            let size = theme('spacing.6')
+            if (name.endsWith('-mini')) {
+              size = theme('spacing.5')
+            } else if (name.endsWith('-micro')) {
+              size = theme('spacing.4')
             }
             return {
               [`--hero-${name}`]: `url('data:image/svg+xml;utf8,${content}')`,
-              "-webkit-mask": `var(--hero-${name})`,
+              '-webkit-mask': `var(--hero-${name})`,
               mask: `var(--hero-${name})`,
-              "mask-repeat": "no-repeat",
-              "background-color": "currentColor",
-              "vertical-align": "middle",
-              display: "inline-block",
+              'mask-repeat': 'no-repeat',
+              'background-color': 'currentColor',
+              'vertical-align': 'middle',
+              display: 'inline-block',
               width: size,
-              height: size,
-            };
-          },
+              height: size
+            }
+          }
         },
-        { values },
-      );
-    }),
-  ],
-};
+        { values }
+      )
+    })
+  ]
+}
