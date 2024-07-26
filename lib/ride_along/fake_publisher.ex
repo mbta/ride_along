e9939dc220129d5f1f3d39f93d74fd4b04d7c95b
Paul Swartz <paul@paulswartz.net>
2024-07-26 16:30:22 -0400

    feat(FakePublisher): rotate through more states

--- lib/ride_along/fake_publisher.ex
+++ lib/ride_along/fake_publisher.ex
@@ -7,7 +7,13 @@ defmodule RideAlong.FakePublisher do
   alias EmqttFailover.Message
   alias Faker.Address, as: FakeAddress
   alias RideAlong.MqttConnection
+
   @default_name __MODULE__
+
+  @trip_id 1234
+  @route_id 2345
+  @vehicle_id "5678"
+
   def start_link(opts) do
     if opts[:start] do
       name = Keyword.get(opts, :name, @default_name)
@@ -17,7 +23,7 @@ defmodule RideAlong.FakePublisher do
     end
   end
 
-  defstruct [:connection, interval: 5_000, topic_prefix: ""]
+  defstruct [:connection, state: :enroute, interval: 15_000, topic_prefix: ""]
 
   @impl GenServer
   def init(_opts) do
@@ -28,7 +34,6 @@ defmodule RideAlong.FakePublisher do
   @impl GenServer
   def handle_continue(:start_timers, state) do
     :timer.send_interval(state.interval, :update)
-    send(self(), :update)
 
     {:noreply, state, {:continue, :connect}}
   end
@@ -47,11 +52,15 @@ defmodule RideAlong.FakePublisher do
 
   @impl GenServer
   def handle_info(:update, state) do
-    for {name, result} <- data() do
+    now = DateTime.shift_zone!(DateTime.utc_now(), Application.get_env(:ride_along, :time_zone))
+
+    {results, new_state} = data(state.state, now)
+
+    for {name, result} <- results do
       publish(state, name, result)
     end
 
-    {:noreply, state}
+    {:noreply, %{state | state: new_state}}
   end
 
   def handle_info({:connected, connection}, %{connection: connection} = state) do
@@ -63,65 +72,126 @@ defmodule RideAlong.FakePublisher do
     {:noreply, state}
   end
 
-  def data do
-    now = DateTime.shift_zone!(DateTime.utc_now(), Application.get_env(:ride_along, :time_zone))
+  def data(state, now)
 
-    trip_id = 1234
-    route_id = 2345
-    vehicle_id = "5678"
+  def data(:enroute, now) do
+    results = %{
+      trips: [trip(now, %{})],
+      locations: [location(now, %{})]
+    }
 
-    %{
-      trips: [
-        %{
-          "Id" => trip_id,
-          "TripDate" => {{now.year, now.month, now.day}, {0, 0, 0, 0}},
-          "RouteId" => List.first(Enum.shuffle([route_id, route_id + 1])),
-          "PickTime" => Calendar.strftime(DateTime.add(now, 30, :minute), "%H:%M"),
-          "PromiseTime" => "0:00",
-          "PickHouseNumber" => FakeAddress.building_number(),
-          "PickAddress1" => FakeAddress.street_name(),
-          "PickAddress2" => FakeAddress.secondary_address(),
-          "PickCity" => FakeAddress.city(),
-          "PickSt" => FakeAddress.state_abbr(),
-          "PickZip" => FakeAddress.zip(),
-          "PickGridX" => "-7106166",
-          "PickGridY" => "4234340",
-          "Anchor" => "P",
-          "PickOrder" => 2,
-          "DropOrder" => 3,
-          "PerformPickup" => 0,
-          "PerformDropoff" => 0
-        }
-      ],
+    next_state = Enum.random([:waiting, :waiting, :arrived, :arrived, :switch_vehicle])
+
+    {results, next_state}
+  end
+
+  def data(:waiting, now) do
+    results = %{
+      trips: [trip(now, %{})],
       locations: [
-        %{
-          "RouteId" => route_id,
-          "VehicleId" => vehicle_id,
-          "Heading" => 90,
-          "Latitude" => Decimal.new("42.346"),
-          "Longitude" => Decimal.new("-71.071"),
-          "LocationDate" =>
-            {{now.year, now.month, now.day}, {now.hour, now.minute, now.second, 0}},
-          "LastPick" => 1,
-          "LastDrop" => 1,
-          "LastArrivedTrip" => nil,
-          "LastDispatchArrivedTrip" => nil
-        },
-        %{
-          "RouteId" => route_id + 1,
-          "VehicleId" => vehicle_id <> "_alt",
-          "Heading" => 90,
-          "Latitude" => Decimal.new("42.346"),
-          "Longitude" => Decimal.new("-71.071"),
-          "LocationDate" =>
-            {{now.year, now.month, now.day}, {now.hour, now.minute, now.second, 0}},
-          "LastPick" => 1,
-          "LastDrop" => 1,
-          "LastArrivedTrip" => nil,
-          "LastDispatchArrivedTrip" => nil
-        }
+        location(now, %{
+          "Latitude" => Decimal.new("42.3434"),
+          "Longitude" => Decimal.new("-71.06166")
+        })
       ]
     }
+
+    {results, :arrived}
+  end
+
+  def data(:arrived, now) do
+    results = %{
+      trips: [trip(now, %{})],
+      locations: [
+        location(now, %{
+          "LastArrivedTrip" => @trip_id
+        })
+      ]
+    }
+
+    {results, :picked_up}
+  end
+
+  def data(:switch_vehicle, now) do
+    results = %{
+      trips: [
+        trip(now, %{
+          "RouteId" => @route_id + 1,
+          "PickOrder" => 3,
+          "DropOrder" => 4
+        })
+      ],
+      locations: [
+        location(now, %{
+          "RouteId" => @route_id + 1,
+          "VehicleId" => "8675"
+        })
+      ]
+    }
+
+    {results, :enroute}
+  end
+
+  def data(:picked_up, now) do
+    result = %{
+      trips: [trip(now, %{})],
+      locations: [
+        location(now, %{
+          "LastArrivedTrip" => @trip_id,
+          "LastPick" => 2
+        })
+      ]
+    }
+
+    {result, :enroute}
+  end
+
+  defp trip(now, updates) do
+    Map.merge(
+      %{
+        "Id" => @trip_id,
+        "TripDate" => {{now.year, now.month, now.day}, {0, 0, 0, 0}},
+        "RouteId" => @route_id,
+        "PickTime" => Calendar.strftime(DateTime.add(now, 30, :minute), "%H:%M"),
+        "PromiseTime" => "0:00",
+        "PickHouseNumber" => FakeAddress.building_number(),
+        "PickAddress1" => FakeAddress.street_name(),
+        "PickAddress2" => FakeAddress.secondary_address(),
+        "PickCity" => FakeAddress.city(),
+        "PickSt" => FakeAddress.state_abbr(),
+        "PickZip" => FakeAddress.zip(),
+        "PickGridX" => "-7106166",
+        "PickGridY" => "4234340",
+        "Anchor" => "P",
+        "PickOrder" => 2,
+        "DropOrder" => 3,
+        "PerformPickup" => 0,
+        "PerformDropoff" => 0
+      },
+      updates
+    )
+  end
+
+  defp location(now, updates) do
+    Map.merge(
+      %{
+        "RouteId" => @route_id,
+        "VehicleId" => @vehicle_id,
+        "Heading" => 90,
+        "Latitude" => Decimal.new("42.346"),
+        "Longitude" => Decimal.new("-71.071"),
+        "LocationDate" => erl_dt(now),
+        "LastPick" => 1,
+        "LastDrop" => 1,
+        "LastArrivedTrip" => nil,
+        "LastDispatchArrivedTrip" => nil
+      },
+      updates
+    )
+  end
+
+  defp erl_dt(%DateTime{} = now) do
+    {{now.year, now.month, now.day}, {now.hour, now.minute, now.second, 0}}
   end
 
   defp publish(%{connection: connection} = state, name, result) when not is_nil(connection) do
