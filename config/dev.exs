1b6b999675c9e30082dd388423c4c67d00f7b68a
Paul Swartz <paul@paulswartz.net>
2024-07-29 15:56:54 -0400

    feat: extract notification into a new server

--- config/dev.exs
+++ config/dev.exs
@@ -46,6 +46,8 @@ config :ride_along, RideAlong.MqttListener, start: true
 
 config :ride_along, RideAlong.EtaMonitor, start: true
 
+config :ride_along, RideAlong.RiderNotifier, start: true
+
 # Enable dev routes for dashboard and mailbox
 config :ride_along, dev_routes: true
 
