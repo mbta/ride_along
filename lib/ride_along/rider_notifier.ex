9ba20ab04f2833229ed151979f2cf40935b43c97
Paul Swartz <paul@paulswartz.net>
2024-08-07 20:23:17 -0400

    feat: RiderNotifier only sends pubsub messages now

--- lib/ride_along/rider_notifier.ex
+++ lib/ride_along/rider_notifier.ex
@@ -1,10 +1,19 @@
 defmodule RideAlong.RiderNotifier do
-  @moduledoc false
+  @moduledoc """
+  Server which listens for updated trips and notifies the user about them.
+
+  Currently, the rider is notified at the first of two events:
+  - they are the next pickup for the vehicle
+  - it is 30 minutes before the promise time
+
+  We keep track locally of which events have been notified. This prevents trival
+  re-notifications, but isn't resilient about restarting the server. We rely on
+  the consumer of the notifications to de-duplicate them in those situations.
+  """
   use GenServer
   require Logger
 
-  alias EmqttFailover.Message
-  alias RideAlong.MqttConnection
+  alias RideAlong.Adept.Trip
 
   @default_name __MODULE__
 
@@ -17,47 +26,72 @@ defmodule RideAlong.RiderNotifier do
     end
   end
 
-  defstruct [:connection, :topic_prefix]
+  defstruct notified_trips: MapSet.new()
   @impl GenServer
   def init(_opts) do
-    Logger.info("#{__MODULE__} starting")
-    state = %__MODULE__{topic_prefix: MqttConnection.topic_prefix() <> "rider_notifier/"}
-    {:ok, state, {:continue, :connect}}
+    state = %__MODULE__{}
+    Phoenix.PubSub.subscribe(RideAlong.PubSub, "trips:updated")
+    Phoenix.PubSub.subscribe(RideAlong.PubSub, "vehicles:updated")
+    {:ok, state}
   end
 
   @impl GenServer
-  def handle_continue(:connect, state) do
-    topics = [state.topic_prefix <> "#"]
-    {:ok, connection} = MqttConnection.start_link(topics)
-    state = %{state | connection: connection}
+  def handle_info(:trips_updated, state) do
+    state = update_trips(state)
     {:noreply, state}
   end
 
-  @impl GenServer
-  def handle_info({:connected, connection}, %{connection: connection} = state) do
+  def handle_info(:vehicles_updated, state) do
+    state = update_trips(state)
     {:noreply, state}
   end
 
-  def handle_info({:message, connection, message}, %{connection: connection} = state) do
-    if message.payload != "" do
-      Logger.info("#{__MODULE__} removing expired topic=#{message.topic}")
+  def update_trips(state) do
+    all_trips = RideAlong.Adept.all_trips()
 
-      MqttConnection.publish(
-        state.connection,
-        %Message{
-          topic: message.topic,
-          # unpublish the retained message
-          payload: "",
-          qos: 1,
-          retain?: true
-        }
-      )
+    all_trips
+    |> relevant_trips(DateTime.utc_now())
+    |> Enum.reduce(state, &maybe_notify/2)
+    |> cleanup_old_notifications(all_trips)
+  end
+
+  defp relevant_trips(all_trips, now) do
+    # status is enroute/waiting (they're the next pickup) or diff is less than
+    # 30m
+    for trip <- all_trips,
+        trip.pick_time != nil,
+        trip.route_id > 0,
+        not trip.pickup_performed?,
+        diff = DateTime.diff(trip.promise_time, now),
+        # we use a loop here to ensure we always set vehicle, even when it's nil
+        vehicle <- [RideAlong.Adept.get_vehicle_by_route(trip.route_id)],
+        diff < 1800 or
+          Trip.status(trip, vehicle, now) in [:enroute, :waiting, :arrived] do
+      trip
     end
-
-    {:noreply, state}
   end
 
-  def handle_info({:disconnected, _, _reason}, state) do
-    {:noreply, state}
+  defp maybe_notify(trip, state) do
+    if MapSet.member?(state.notified_trips, trip.trip_id) do
+      state
+    else
+      send_notification(state, trip)
+    end
+  end
+
+  defp send_notification(state, trip) do
+    Phoenix.PubSub.local_broadcast(
+      RideAlong.PubSub,
+      "notification:trip",
+      {:trip_notification, trip}
+    )
+
+    %{state | notified_trips: MapSet.put(state.notified_trips, trip.trip_id)}
+  end
+
+  defp cleanup_old_notifications(state, all_trips) do
+    # only keep trip IDs which are still in the `all_trips` list
+    trip_ids = MapSet.new(all_trips, & &1.trip_id)
+    %{state | notified_trips: MapSet.intersection(state.notified_trips, trip_ids)}
   end
 end
---
+++ test/ride_along/rider_notifier_test.exs
@@ -0,0 +1,58 @@
+defmodule RideAlong.RiderNotifierTest do
+  @moduledoc false
+  use ExUnit.Case
+
+  alias RideAlong.Adept
+  alias RideAlong.AdeptFixtures
+
+  setup do
+    Phoenix.PubSub.subscribe(RideAlong.PubSub, "notification:trip")
+
+    on_exit(fn ->
+      Adept.set_vehicles([])
+      Adept.set_trips([])
+    end)
+
+    :ok
+  end
+
+  test "triggers a notification when the trip is the next pickup" do
+    trip_id = :erlang.unique_integer()
+
+    Adept.set_vehicles([AdeptFixtures.vehicle_fixture()])
+    Adept.set_trips([AdeptFixtures.trip_fixture(%{trip_id: trip_id})])
+
+    assert_receive {:trip_notification, %Adept.Trip{trip_id: ^trip_id}}
+  end
+
+  test "triggers a notification when the trip is promised in the next 30m" do
+    trip_id = :erlang.unique_integer()
+
+    Adept.set_trips([
+      AdeptFixtures.trip_fixture(%{trip_id: trip_id, promise_time: DateTime.utc_now()})
+    ])
+
+    assert_receive {:trip_notification, %Adept.Trip{trip_id: ^trip_id}}
+  end
+
+  test "does not trigger a notification when the pickup is not next and the promise time is further in the future" do
+    trip_id = :erlang.unique_integer()
+
+    Adept.set_trips([AdeptFixtures.trip_fixture(%{trip_id: trip_id})])
+
+    refute_receive {:trip_notification, %Adept.Trip{trip_id: ^trip_id}}
+  end
+
+  test "does not re-trigger a notification" do
+    trip_id = :erlang.unique_integer()
+
+    Adept.set_vehicles([AdeptFixtures.vehicle_fixture()])
+    Adept.set_trips([AdeptFixtures.trip_fixture(%{trip_id: trip_id})])
+
+    assert_receive {:trip_notification, _}
+
+    Adept.set_vehicles([AdeptFixtures.vehicle_fixture()])
+
+    refute_receive {:trip_notification, %Adept.Trip{trip_id: ^trip_id}}
+  end
+end
